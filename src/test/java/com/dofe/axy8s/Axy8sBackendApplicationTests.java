b48f1d9e3796a9094dac2a23ce85496f202af3a2
Jenkins Controller <jenkins@trainingdevsecops.com>
2025-12-11 02:04:11 +0000

    Update file Axy8sBackendApplicationTests.java

--- src/test/java/com/dofe/axy8s/Axy8sBackendApplicationTests.java
+++ src/test/java/com/dofe/axy8s/Axy8sBackendApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * Smoke test: đảm bảo Spring context khởi động được.
