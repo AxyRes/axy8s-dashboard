aab40211127b7d73c755e18f0fe2172e1fc6cbbe
Jenkins Controller <jenkins@trainingdevsecops.com>
2025-12-11 01:48:08 +0000

    Update file Axy8sBackendApplicationTests.java

--- src/test/java/com/dofe/axy8s/Axy8sBackendApplicationTests.java
+++ src/test/java/com/dofe/axy8s/Axy8sBackendApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
  * Smoke test: đảm bảo Spring context khởi động được.
  */
 @SpringBootTest
+@ActiveProfiles("test")
 class Axy8sBackendApplicationTests {
 
     // Mock KubernetesClient để test không cần connect cluster thật
