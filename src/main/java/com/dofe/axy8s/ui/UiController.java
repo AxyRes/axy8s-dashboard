47c5026c243c56ad3d1a0ac267257ded06bbfb09
Jenkins Controller <jenkins@trainingdevsecops.com>
2025-12-11 13:01:23 +0000

    Update file UiController.java

--- src/main/java/com/dofe/axy8s/ui/UiController.java
+++ src/main/java/com/dofe/axy8s/ui/UiController.java
@@ -1,31 +1,18 @@
 package com.dofe.axy8s.ui;
 
-import com.dofe.axy8s.ui.config.UiConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UiController {
 
-    @Autowired
-    private UiConfig uiConfig;
-
-    private void ensureEnabled() {
-        if (!uiConfig.isUiEnabled()) {
-            throw new RuntimeException("UI is disabled");
-        }
-    }
-
     @GetMapping("/ui/login")
     public String loginPage() {
-        ensureEnabled();
         return "ui/login";
     }
 
     @GetMapping("/ui")
     public String dashboard() {
-        ensureEnabled();
         return "ui/dashboard";
     }
 }
